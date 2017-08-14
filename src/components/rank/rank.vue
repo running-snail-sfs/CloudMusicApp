0f3ff6bbf29acdd35b2b3bf3a817cfcec71bc6af
jsonshao <742410510@qq.com>
2017-08-14 22:21:33 +0800

    增加的排行榜列表

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,15 +1,112 @@
 <template>
-  <div class="recommend" ref="recommend">
-    推荐rank
+  <div class="rank" ref="rank">
+    <Scroll class="rank-item" :data="rankList">
+      <div class="scroll_contain">
+        <ul>
+          <li v-for="item in rankList">
+            <div class="item-content">
+              <div class="item_img">
+                <img :src="item.picUrl" width="80">
+              </div>
+              <div class="img_text">
+                <h1>{{item.topTitle}}</h1>
+                <div class="song-item" v-for="(songItem ,index) in item.songList">{{index + 1}}<span class="song-name">{{songItem.songname}}</span>-{{songItem.singername}}
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="item_bottom"></div>
+      </div>
+
+    </Scroll>
   </div>
 </template>
 
 <script>
-  export default{}
+  import { getTopList } from 'api/rank'
+  import Scroll from 'base/scroll/scroll'
+
+  export default {
+    data () {
+      return {
+        rankList: []
+      }
+    },
+    created () {
+      this._getRankList()
+    },
+    methods: {
+      _getRankList () {
+        var _this = this
+        getTopList().then((data) => {
+          _this.rankList = data.data.topList
+          console.log(_this.rankList)
+        })
+      }
+    },
+    components: {
+      Scroll
+
+    }
+  }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+<style scoped>
+  .rank {
+    background: black;
+    width: 100%;
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+  }
 
+  .scroll_contain li {
+    margin: 20px;
+  }
+
+  .rank-item {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  .item_bottom{
+   height: 50px;
+    width: 100%;
+  }
+
+  .item-content {
+    display: flex;
+  }
+
+  .item_img {
+    flex: 1;
+  }
+
+  .img_text {
+    flex: 3;
+  }
+
+  .img_text h1 {
+    margin: 0 0 0 10px;
+    font-size: 18px;
+    color: yellow;
+    font-weight: bold;
+  }
+
+  .song-item {
+    margin: 6px 0 0 10px;
+    font-family: "hiragino sans gb";
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+
+  }
+
+  .song-name {
+    margin: 0 0 0 5px;
+  }
 
 </style>
+
